1f57ac632cfe1dc2ce13a16cac0bb7880e9978c8
Felipe Rodrigues <65414548+lferodrigues@users.noreply.github.com>
2022-11-28 16:24:31 -0300

    Add files via upload

---
+++ 07-operadores-de-comparacao.py
@@ -0,0 +1,37 @@
+#sao operadores utilizados para comparar dois ou mais valores.
+saldo = 450
+saque = 200
+
+#retorno de valor booleano
+#testando a igualdade
+print(saldo == saque)
+
+print("---")
+
+saldo = 450
+saque = 200
+#testando a diferença
+print( saldo != saque)
+
+print("---")
+
+#testando maior / maior ou igual
+
+#testando maior
+print( saldo > saque)
+
+print("---")
+
+print(saldo >= saque)
+
+print("---")
+
+#testando menor / menor igual
+saldo = 450
+saque = 200
+
+#testando menor
+print( saldo < saque)
+
+#testando menor ou igual
+print(saldo<= saque)
---
+++ 08-operadores de atribuicao.py
@@ -0,0 +1,47 @@
+#São operadores utlizados para definir o valor inicial ou sobrescrever o valor de uma variavel.
+
+#atribuição simples
+saldo=500
+
+print(saldo)
+print("-----")
+#atribuição com adição
+saldo = 500
+saldo += 200
+
+print(saldo)
+print("----")
+#atribuiçaõ com subtração
+saldo = 500
+saldo -= 100
+
+print(saldo)
+print("----")
+#atribuição com multiplicação
+saldo = 500
+saldo *=2
+
+print(saldo)
+print("----")
+#atribuição com divisão
+saldo = 500
+saldo //=2
+
+print(saldo)
+print("ou")
+saldo = 500
+saldo /=2
+
+print(saldo)
+print("---")
+#atribuição com módulo
+saldo = 500
+saldo %= 480
+
+print(saldo)
+print("----")
+#atribuição com exponenciação
+saldo = 80
+saldo **= 2
+
+print(saldo)
